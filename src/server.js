8095cab7b04dc4599ddf47e06b4a70231efd76e2
Murilo Costa <murilocoimbracosta@gmail.com>
2024-06-19 17:49:42 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require("dotenv/config")
+
 require("express-async-errors");
 
 const migrationsRun = require("./database/sqlite/migrations");
@@ -35,5 +37,5 @@ app.use((error, request, response, next) => {
     
 })
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
